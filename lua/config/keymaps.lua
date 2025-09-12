a03270d2080e86f56dc81abcf633872d17b37903
Your Name <you@example.com>
2025-09-12 12:27:56 +0800

    enhance bufffline

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -74,6 +74,15 @@ map("n", "<leader>fl", "<cmd>BLines<CR>", { desc = "Search in Lines", noremap = 
 map("n", "<leader>ft", "<cmd>BTags<CR>", { desc = "Search in Tags", noremap = true, silent = true })
 
 -- Buffer and file operations
+map("n", "<leader>1", ":BufferLineGoToBuffer 1<CR>", { desc = "Go to buffer 1", noremap = true, silent = true })
+map("n", "<leader>2", ":BufferLineGoToBuffer 2<CR>", { desc = "Go to buffer 2", noremap = true, silent = true })
+map("n", "<leader>3", ":BufferLineGoToBuffer 3<CR>", { desc = "Go to buffer 3", noremap = true, silent = true })
+map("n", "<leader>4", ":BufferLineGoToBuffer 4<CR>", { desc = "Go to buffer 4", noremap = true, silent = true })
+map("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>", { desc = "Go to buffer 5", noremap = true, silent = true })
+map("n", "<leader>6", ":BufferLineGoToBuffer 6<CR>", { desc = "Go to buffer 6", noremap = true, silent = true })
+map("n", "<leader>7", ":BufferLineGoToBuffer 7<CR>", { desc = "Go to buffer 7", noremap = true, silent = true })
+map("n", "<leader>8", ":BufferLineGoToBuffer 8<CR>", { desc = "Go to buffer 8", noremap = true, silent = true })
+map("n", "<leader>9", ":BufferLineGoToBuffer 9<CR>", { desc = "Go to buffer 9", noremap = true, silent = true })
 map("n", "<leader>qq", "<cmd>Bw<CR>", { desc = "Quit Buffer", noremap = true, silent = true })
 map("n", "<leader>gf", "<cmd>GFiles<CR>", { desc = "Git Files", noremap = true, silent = true })
 map("n", "<leader>bd", "<cmd>Bw<CR>", { desc = "Delete Buffer", noremap = true, silent = true })
