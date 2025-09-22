c198174757bc6cc882cfb136c8b40dc7ee4e5bc6
Your Name <you@example.com>
2025-09-22 10:26:35 +0800

    build(lazy-lock): update plugin commit hashes and add new plugins
    - Update LazyVim, blink.cmp, flash.nvim, gitsigns.nvim, lazydev.nvim,
      mason.nvim, mason-lspconfig.nvim, nvim-lint, nvim-lspconfig,
      nvim-treesitter, nvim-treesitter-textobjects, snacks.nvim,
      tokyonight.nvim, and trouble.nvim to latest commits
    - Add SchemaStore.nvim, auto-save.nvim, clangd_extensions.nvim,
      cmake-tools.nvim, luarocks.nvim, venv-selector.nvim
    
    feat(lazyvim): add formatting and language support extras
    
    - Enable formatting plugins: biome, black, prettier
    - Add language support for clangd, cmake, json, python
    
    feat(config): disable python linting and autoformatting
    
    - Set vim.g.lazyvim_linting_python to false
    - Set vim.g.autoformat to false
    
    feat(plugins): add development plugins and configurations
    
    - Add blink.cmp with codeium integration
    - Integrate luarocks.nvim for Lua package management
    - Configure auto-save.nvim for automatic file saving
    - Setup conform.nvim with autopep8 for python formatting
    
    feat(plugins): add go language support
    
    - Include go in treesitter ensure_installed list

--- lua/plugins/dev.lua
+++ lua/plugins/dev.lua
@@ -1,6 +1,47 @@
 return {
   { "voldikss/vim-floaterm" },
   {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "codeium.nvim", "saghen/blink.compat" },
+    opts = {
+      sources = {
+        compat = { "codeium" },
+        providers = {
+          codeium = {
+            kind = "Codeium",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
+    config = true,
+  },
+  {
+    "okuuva/auto-save.nvim",
+    version = "^1.0.0",                       -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+    cmd = "ASToggle",                         -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        python = { "autopep8" },
+      },
+    },
+  },
+  {
     "nvim-mini/mini.comment",
     config = function()
       require("mini.comment").setup({
